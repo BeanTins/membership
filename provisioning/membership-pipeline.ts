c9e6d82ed15e74a4f4d8b62daa330534e4b28ab5
gazblair <gazblair@hotmail.com>
2022-03-07 17:09:50 +0000

    workaround for top level await

--- provisioning/membership-pipeline.ts
+++ provisioning/membership-pipeline.ts
@@ -5,42 +5,47 @@ import { PipelineBuilder } from "./pipeline-builder/pipeline-builder"
 import { MembershipFactory} from "./membership-factory"
 import { StageParameters } from "../infrastructure/stage-parameters"
 
-const memberTableArn = Fn.importValue("MemberTableArntest")
-const userPoolArn = await new StageParameters("us-east-1").retrieveFromStage("UserPoolArn", "test")
+async function main(): Promise<void> 
+{
+  const memberTableArn = Fn.importValue("MemberTableArntest")
+  let userPoolArn = await new StageParameters("us-east-1").retrieveFromStage("UserPoolArn", "test")
 
-const membershipFactory = new MembershipFactory()
+  const membershipFactory = new MembershipFactory()
 
-const app = new App()
+  const app = new App()
 
-const pipeline = new PipelineBuilder(app, membershipFactory)
+  const pipeline = new PipelineBuilder(app, membershipFactory)
 
-pipeline.withName("MembershipPipeline")
+  pipeline.withName("MembershipPipeline")
 
-pipeline.withCommitStage(
-  {
-    extractingSourceFrom: { provider: SCM.GitHub, owner: "BeanTins", repository: "membership", branch: "main" },
-    executingCommands: ["npm ci", "npm run build", "npm run test:unit", "npx cdk synth"],
-    reporting: {fromDirectory: "reports/unit-tests", withFiles: ["test-results.xml"]},
-  })
-pipeline.withAcceptanceStage(
-  {
-    extractingSourceFrom: {provider: SCM.GitHub, owner: "BeanTins", repository: "membership", branch: "main"},
-    executingCommands: ["npm ci", "npm run test:component"],
-    reporting: {fromDirectory: "reports/component-tests", withFiles: ["test-results.xml", "tests.log"], exportingTo: ExportType.S3},
-    exposingEnvVars: true,
-    withPermissionToAccess: [
-      {resource: memberTableArn, withAllowableOperations: ["dynamodb:*"]},
-      {resource: "*", withAllowableOperations: ["ssm:GetParameter"]},
-      {resource: userPoolArn, withAllowableOperations: ["cognito-idp:*"]}]
-  }
-)
+  pipeline.withCommitStage(
+    {
+      extractingSourceFrom: { provider: SCM.GitHub, owner: "BeanTins", repository: "membership", branch: "main" },
+      executingCommands: ["npm ci", "npm run build", "npm run test:unit", "npx cdk synth"],
+      reporting: {fromDirectory: "reports/unit-tests", withFiles: ["test-results.xml"]},
+    })
+  pipeline.withAcceptanceStage(
+    {
+      extractingSourceFrom: {provider: SCM.GitHub, owner: "BeanTins", repository: "membership", branch: "main"},
+      executingCommands: ["npm ci", "npm run test:component"],
+      reporting: {fromDirectory: "reports/component-tests", withFiles: ["test-results.xml", "tests.log"], exportingTo: ExportType.S3},
+      exposingEnvVars: true,
+      withPermissionToAccess: [
+        {resource: memberTableArn, withAllowableOperations: ["dynamodb:*"]},
+        {resource: "*", withAllowableOperations: ["ssm:GetParameter"]},
+        {resource: userPoolArn, withAllowableOperations: ["cognito-idp:*"]}]
+    }
+  )
 
-pipeline.withProductionStage(
-  {
-    manualApproval: true,
-  }
-)
+  pipeline.withProductionStage(
+    {
+      manualApproval: true,
+    }
+  )
 
-pipeline.build()
+  pipeline.build()
 
-app.synth()
+  app.synth()
+}
+
+main().catch(console.error)
